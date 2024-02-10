33cb5b46e18eb211d2b7860118016005e7680be4
Meher Malladi <rm.meher97@gmail.com>
2024-02-10 19:16:51 +0100

    add dependency finding

--- 3rdparty/dependencies.cmake
+++ 3rdparty/dependencies.cmake
@@ -1,4 +1,24 @@
-# should handle find_package or fetch content make availabe all necessary dependencies.
-# see kiss icp for reference. but i want my own stuff.
-find_package(Eigen3 REQUIRED NO_MODULE)
-include(${CMAKE_CURRENT_LIST_DIR}/cli11/cli11.cmake)
+# should handle find_package or fetch content make availabe all necessary
+# dependencies. see kiss icp for reference. but i want my own stuff.
+
+# in the end it ended up pretty much the same as the kiss icp one. surprise.
+function(find_dep PACKAGE_NAME TARGET_NAME FETCH_CMAKE_FILE)
+  string(TOUPPER ${PACKAGE_NAME} PACKAGE_NAME_UP)
+  set(USE_FROM_SYSTEM_OPTION "USE_SYSTEM_${PACKAGE_NAME_UP}")
+  if(${${USE_FROM_SYSTEM_OPTION}})
+    message(STATUS "Searching for ${PACKAGE_NAME} in system. ")
+    find_package(${PACKAGE_NAME} QUIET)
+  endif()
+  if(NOT TARGET ${TARGET_NAME})
+    message(STATUS "${PACKAGE_NAME}'s target: ${TARGET_NAME} not found.")
+    if(NOT ${USE_FROM_SYSTEM_OPTION})
+      message(STATUS "Fetching it using FetchContent (${FETCH_CMAKE_FILE})")
+    endif()
+    include(${FETCH_CMAKE_FILE})
+  endif()
+endfunction()
+
+option(USE_SYSTEM_EIGEN3 "Use system Eigen" OFF)
+find_dep("Eigen3" "Eigen3::Eigen" "${CMAKE_CURRENT_LIST_DIR}/eigen/eigen.cmake")
+option(USE_SYSTEM_CLI11 "Use system CLI11" OFF)
+find_dep("CLI11" "CLI11::CLI11" "${CMAKE_CURRENT_LIST_DIR}/cli11/cli11.cmake")
--- 3rdparty/eigen/eigen.cmake
+++ 3rdparty/eigen/eigen.cmake
@@ -1,1 +1,22 @@
-# need to populate with fetch content stuff like cli11
+set(EIGEN_BUILD_DOC
+    OFF
+    CACHE BOOL "Don't build Eigen docs")
+set(EIGEN_BUILD_TESTING
+    OFF
+    CACHE BOOL "Don't build Eigen tests")
+set(EIGEN_BUILD_PKGCONFIG
+    OFF
+    CACHE BOOL "Don't build Eigen pkg-config")
+set(EIGEN_BUILD_BLAS
+    OFF
+    CACHE BOOL "Don't build blas module")
+set(EIGEN_BUILD_LAPACK
+    OFF
+    CACHE BOOL "Don't build lapack module")
+
+include(FetchContent)
+FetchContent_Declare(
+  eigen
+  GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+  GIT_TAG 287c8017808fb4c7a651ba4c02363a773e2f0c46) # hash for v 3.4 commit
+FetchContent_MakeAvailable(eigen)
