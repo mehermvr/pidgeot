eecbb725fa7692669e2a9b4f7d7e2a532774d727
Meher Malladi <meher.malladi@uni-bonn.de>
2024-02-15 00:27:22 +0100

    pb_utils hash update

--- 3rdparty/pb_utils/pb_utils.cmake
+++ 3rdparty/pb_utils/pb_utils.cmake
@@ -2,5 +2,5 @@ include(FetchContent)
 FetchContent_Declare(
   pb_utils
   GIT_REPOSITORY https://github.com/mehermvr/pb_utils.git
-  GIT_TAG 0cda096)
+  GIT_TAG fd5dc42)
 FetchContent_MakeAvailable(pb_utils)
