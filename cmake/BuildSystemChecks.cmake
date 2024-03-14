433425b9910335c463a79aa98b69680a3617cf4f
Meher Malladi <meher.malladi@uni-bonn.de>
2024-03-14 10:50:27 +0100

    warn if not ninja, fatal if not gcc 14

--- cmake/BuildSystemChecks.cmake
+++ cmake/BuildSystemChecks.cmake
@@ -5,23 +5,27 @@ if(NOT
    STREQUAL
    "Ninja")
   message(
-    FATAL_ERROR
+    # FATAL_ERROR
+    AUTHOR_WARNING
       "This project requires the Ninja generator. Refers to https://cmake.org/cmake/help/latest/manual/cmake-cxxmodules.7.html#generator-support"
   )
 endif()
 
-# ensure clang is being used
-execute_process(COMMAND ${CMAKE_CXX_COMPILER} --version OUTPUT_VARIABLE clang_full_version_string)
+# ensure ~~clang~~ gcc-14 is being used
+# execute_process(COMMAND ${CMAKE_CXX_COMPILER} --version OUTPUT_VARIABLE clang_full_version_string)
+execute_process(COMMAND ${CMAKE_CXX_COMPILER} --version OUTPUT_VARIABLE gcc_full_version_string)
+#   REPLACE ".*clang version ([0-9]+\\.[0-9]+).*"
+#           "\\1"
 string(
   REGEX
-  REPLACE ".*clang version ([0-9]+\\.[0-9]+).*"
+  REPLACE ".*GCC\\) ([0-9]+\\.[0-9]+).*"
           "\\1"
-          CLANG_VERSION_STRING
-          ${clang_full_version_string})
-if(CLANG_VERSION_STRING VERSION_GREATER 16)
+          GCC_VERSION_STRING
+          ${gcc_full_version_string})
+if(GCC_VERSION_STRING VERSION_GREATER_EQUAL 14)
   # Print CMake version and project name
-  message(STATUS "Building ${PROJECT_NAME} with CMake version: ${CMAKE_VERSION} On CLANG-${CLANG_VERSION_STRING}")
+  message(STATUS "Building ${PROJECT_NAME} with CMake version: ${CMAKE_VERSION} On GCC-${GCC_VERSION_STRING}")
 
 else()
-  message(FATAL_ERROR "Please use clang version 17.0 and above")
+  message(FATAL_ERROR "Please use gcc version 14.0 and above")
 endif()
